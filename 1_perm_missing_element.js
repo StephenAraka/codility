a3c18f5d4424faaff71299fb6e7bd3fa5a1faef2
StephenAraka <stevenaraka@gmail.com>
2022-03-19 10:47:21 +0200

    solution 3 - v2

--- 1_perm_missing_element.js
+++ 1_perm_missing_element.js
@@ -3,14 +3,14 @@ let array = [7, 3, 2, 8, 5, 4, 1]
 
 const findMissingNum = (arr) => {
   let length = arr.length + 1;
-  let sum = 0;
-  let current = arr.reduce((partialSum, accumulated) => partialSum + accumulated, 0);
+  let expectedSum = 0;
+  // let currentSum = arr.reduce((partialSum, accumulated) => partialSum + accumulated, 0); // or
+  let currentSum = 0;
+  arr.forEach(number => currentSum += number);
 
-  for (let i = 1; i <= length; i++) {
-    sum += i
-  }
+  expectedSum = length * (length + 1) / 2;
 
-  return sum - current;
+  return expectedSum - currentSum;
 }
 
 console.log(findMissingNum(array))
