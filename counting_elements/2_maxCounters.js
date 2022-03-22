ed1aa1e5ede5dbf227ddff38ff2d70d648e62488
StephenAraka <stevenaraka@gmail.com>
2022-03-22 10:49:59 +0200

    Max Counters - Solution 2 better

--- counting_elements/2_maxCounters.js
+++ counting_elements/2_maxCounters.js
@@ -2,14 +2,26 @@
 
 const maxCounters = (n, a) => {
   let maxArray = new Array(n).fill(0);
+  let startline = 0;
+  let current_max = 0;
 
-  a.forEach((element, index) => {
-    if (element <= n) {
-      maxArray[element - 1] = maxArray[element - 1] + 1;
+  a.forEach((incrementVal) => {
+    let index = incrementVal - 1;
+
+    if (incrementVal > n) {
+      startline = current_max;
+    } else if (maxArray[index] < startline) {
+      maxArray[index] = startline + 1;
     } else {
-      maxArray.fill(Math.max(...maxArray));
+      maxArray[index] += 1;
     }
+
+    if (incrementVal <= n && maxArray[index] > current_max) current_max = maxArray[index];
   });
+
+  for (let i = 0; i < n; i++) {
+    if (maxArray[i] < startline) maxArray[i] = startline;
+  }
 
   return maxArray;
 }
