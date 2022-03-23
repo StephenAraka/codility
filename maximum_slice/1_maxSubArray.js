4a8226e1d4fd424e4f072d642190860d983e6b90
StephenAraka <stevenaraka@gmail.com>
2022-03-23 14:34:34 +0200

    Solution - best

--- maximum_slice/1_maxSubArray.js
+++ maximum_slice/1_maxSubArray.js
@@ -1,17 +1,13 @@
 let input = [23171, 21011, 21123, 21366, 21013, 21367];
 
-const maxProfit = (input) => {
-  let array = [];
-
-  for (let i = 0; i < input.length - 1; i++) {
-    array.push(input[i + 1] - input[i]);
-  }
-
-  let globalMax = array[0];
-  let localMax = array[0];
+const maxProfit = (array) => {
+  let globalMax = 0;
+  let localMax = 0;
 
   for (let i = 1; i < array.length; i++) {
-    localMax = Math.max(array[i], localMax + array[i]);
+    let diff = array[i] - array[i - 1];
+
+    localMax = Math.max(diff, localMax + diff);
     globalMax = Math.max(localMax, globalMax);
   }
 
